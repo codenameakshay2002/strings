6b45e06f9963173cde9918753cc8f46c34219e24
Akshay Naik <117588527+codenameakshay2002@users.noreply.github.com>
2025-11-03 23:07:15 +0530

    Count characters in a vector instead of null-terminated
    Fix character array length counting logic.

--- count_characterarray.cpp
+++ count_characterarray.cpp
@@ -1,3 +1,4 @@
+// Find the length of the string 
 #include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
@@ -6,6 +7,7 @@ int main(){
     vector<char>a={'s','s','w','w'};
     int i=0;
     int count=0;
+    //if the index is not equal null value increase the value of the count
     while(a[i]!='\0'){
         count++;
         i++;
@@ -13,3 +15,4 @@ int main(){
     cout<<count;
     return 0;
 }
+// out should be 4
